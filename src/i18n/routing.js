f44cb4233aabe273e1a006ca11892dc83bd70d41
DontMove225 <dontmove225@gmail.com>
2026-08-07 16:50:11 +0200

    Update routing.js

--- src/i18n/routing.js
+++ src/i18n/routing.js
@@ -1,7 +1,9 @@
 import { defineRouting } from 'next-intl/routing';
+
 export const locales = ['fr','en','de','es','it','pt','pl','is','ja','ko','zh','ar','th'];
+
 export const routing = defineRouting({
   locales,
   defaultLocale: 'fr',
-  localePrefix: 'always',   // ← était 'as-needed'
+  localePrefix: 'always',   // ← était 'as-needed' : c'est LA correction
 });
