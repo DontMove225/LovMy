650452598d650d0fb76cad1ccbf2cd2f65dff80d
DontMove225 <dontmove225@gmail.com>
2026-08-07 16:09:14 +0200

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -4,5 +4,9 @@ import { routing } from './i18n/routing';
 export default createMiddleware(routing);
 
 export const config = {
-  matcher: ['/', '/(fr|en|de|es|it|pt|pl|is|ja|ko|zh|ar|th)/:path*', '/((?!api|_next|_vercel|.*\\..*).*)']
+  matcher: [
+    '/',
+    '/(fr|en|de|es|it|pt|pl|is|ja|ko|zh|ar|th)/:path*',
+    '/((?!api|admin|_next|_vercel|.*\\..*).*)'
+  ]
 };
