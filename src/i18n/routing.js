b2eab420d52230c729647281491ff4ee5eff8731
DontMove225 <dontmove225@gmail.com>
2026-08-07 14:22:58 +0200

    Clean up routing.js by removing whitespace
    Removed unnecessary whitespace and comments in routing.js.

--- src/i18n/routing.js
+++ src/i18n/routing.js
@@ -1,7 +1,5 @@
 import { defineRouting } from 'next-intl/routing';
-
 export const locales = ['fr','en','de','es','it','pt','pl','is','ja','ko','zh','ar','th'];
-
 export const routing = defineRouting({
   locales,
   defaultLocale: 'fr',
